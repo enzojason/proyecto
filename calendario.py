c53b083e63e587fb1c6c2016a026813b3ab545f5
enzojason <124959806+jonataman@users.noreply.github.com>
2023-03-14 15:10:42 -0300

    nuevos cambios

---
+++ calendario.py
@@ -0,0 +1,19 @@
+print("calendar")
+import calendar
+import locale
+locale.setlocale(locale.LC_ALL, '')
+#meses
+#imprime los meses y las abreviaturas
+for i in range(1,13):
+    print(calendar.month_abbr[i],calendar.month_name[i])
+
+#dias
+for i in range(0,7):
+    print(calendar.day_abbr[i],calendar.day_name[i])
+
+print(calendar.monthrange(2023,3))
+print(calendar.monthcalendar(2023,3))
+print(calendar.weekheader(10))
+
+for x in calendar.monthcalendar(2023,3):
+    print(x)
--- pruebas.py
+++ pruebas.py
@@ -1,15 +1,26 @@
 
 #from tkinter import * 
 #raiz = Tk()
-
+import calendar
+import locale
+import datetime
 import tkinter as tk
 raiz=tk.Tk()
-
 raiz.title("Calendario") #Cambiar el nombre de la ventana
 raiz.geometry("500x400")
 raiz.iconbitmap("calendario.ico") #Cambiar el icono
 #raiz.config(bg="blue") #Cambiar color de fondo
 raiz.resizable(0,0) #si la ventana es manipulable de x,y 0,0=NO redimencionar
+
+#label fecha actual
+locale.setlocale(locale.LC_ALL, '')
+from tkinter import *
+x=datetime.datetime.now()
+fecha=x.strftime("%A %d/%m/%Y")
+labelfecha=Label(raiz,text=fecha)
+labelfecha.place(x=325,y=5)
+labelfecha.config(fg="black",font=("Verdana",12))
+
 
 #FRAME
 from tkinter import *
@@ -19,11 +30,13 @@ mi_Frame.config(bg="grey")
 mi_Frame.config(width="550",height="320")
 #mi_frame.config(cursor=),se edita el cursor
 
-#label
+#label encabezado fecha
 lunes=Label(mi_Frame,text="Lunes Martes Miercoles Jueves Viernes Sabado Domingo")
 lunes.place(x=5,y=15)
 lunes.config(fg="black",bg="white",font=("Verdana",12))
 
+
+#dias de la semana labels
 
 
 raiz.mainloop()
